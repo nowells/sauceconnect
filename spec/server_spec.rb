121b10d4f7a9a66bef5b07cb84a46ee09cf244f9
Julian C. Dunn <jdunn@opscode.com>
2013-06-05 22:45:58 -0500

    Fix my tests for the new chefspec syntax

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -2,30 +2,28 @@ require 'spec_helper'
 
 describe 'sauceproxy::server' do
 
-  before do
-    Fauxhai.mock(platform:'centos', version:'6.3')
+  let (:chef_run) do
+    runner = ChefSpec::ChefRunner.new(
+      platform: 'centos',
+      version: '6.3'
+    )
+    runner.node.set['sauceproxy'] = Hash.new
+    runner.node.set['sauceproxy']['server'] = Hash.new
+    runner.node.set['sauceproxy']['server']['user'] = 'fake'
+    runner.node.set['sauceproxy']['server']['install_dir'] = '/tmp/fake'
+    runner.converge('sauceproxy::server')
   end
 
   it 'should create a sauceproxy directory with the right ownership' do
-    chef_run = ChefSpec::ChefRunner.new do |node|
-      node.set['sauceproxy'] = Hash.new
-      node.set['sauceproxy']['server'] = Hash.new
-      node.set['sauceproxy']['server']['user'] = 'fake'
-      node.set['sauceproxy']['server']['install_dir'] = '/tmp/fake'
-    end
-    chef_run.converge 'sauceproxy::server'
-    chef_run.should create_directory '/tmp/fake'
-    chef_run.directory('/tmp/fake').should be_owned_by('fake')
+    expect(chef_run).to create_directory('/tmp/fake')
+    expect(chef_run.directory('/tmp/fake')).to be_owned_by('fake')
 #    Haven't got this working yet. Need to figure out how to retrieve Chef::Config[:file_cache_path]
 #    from within RSpec
 #    chef_run.should create_remote_file '/var/cache/chef/Sauce-Connect-3.0-r21.zip'
   end
 
   it 'should start a service called sauceproxy' do
-    chef_run = ChefSpec::ChefRunner.new
-    chef_run.converge 'sauceproxy::server'
-    chef_run.should create_file '/etc/init.d/sauceproxy'
-    chef_run.should set_service_to_start_on_boot 'sauceproxy'
-    chef_run.should start_service 'sauceproxy'
+    expect(chef_run).to start_service('sauceproxy')
+    expect(chef_run).to set_service_to_start_on_boot('sauceproxy')
   end
 end
