847c957826786862fec68fd0601595a93443e3ad
Julian C. Dunn <jdunn@getchef.com>
2014-03-31 11:48:45 -0400

    Fix coverage, and reorganize specs based on @sethvargo suggestions

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -14,37 +14,47 @@ describe 'sauceconnect::server' do
     runner.converge('sauceconnect::server')
   end
 
-  it 'should install unzip to unpack the zipfile' do
-    expect(chef_run).to install_package('unzip')
+  context 'setting up the system' do
+
+    it 'installs unzip to unpack the zipfile' do
+      expect(chef_run).to install_package('unzip')
+    end
+
+    it 'creates a user to run sauceconnect under' do
+      expect(chef_run).to create_user('fake')
+    end
+
+    it 'creates a sauceconnect directory with the right ownership' do
+      expect(chef_run).to create_directory('/tmp/fake').with(:user => 'fake')
+    end
+
+    it 'creates a sysconfig file' do
+      expect(chef_run).to create_template('/etc/sysconfig/sauceconnect').with(:user => 'root', :group => 'root', :mode => 00644)
+    end
+
+    it 'creates an init script' do
+      expect(chef_run).to create_template('/etc/init.d/sauceconnect').with(:user => 'root', :group => 'root', :mode => 00755)
+    end
   end
 
-  it 'should create a user to run sauceconnect under' do
-    expect(chef_run).to create_user('fake')
-  end
-
-  it 'should create a sauceconnect directory with the right ownership' do
-    expect(chef_run).to create_directory('/tmp/fake').with(:user => 'fake')
-  end
-
-  it 'should create a sysconfig file' do
-    expect(chef_run).to create_template('/etc/sysconfig/sauceconnect').with(:user => 'root', :group => 'root', :mode => 00644)
-  end
-
-  it 'should create an init script' do
-    expect(chef_run).to create_template('/etc/init.d/sauceconnect').with(:user => 'root', :group => 'root', :mode => 00755)
-  end
-
-  it 'should download a remote_file from Sauce Labs' do
-    expect(chef_run).to create_remote_file(::File.join(Chef::Config[:file_cache_path], 'Sauce-Connect-3.14159.zip'))
-    expect(chef_run).to notify('execute[unzip-saucelabs-proxy]').to(:run).immediately
-  end
-
-  it 'should extract the payload' do
-    expect(chef_run).to run_execute('unzip-saucelabs-proxy')
+  context 'downloading from SauceLabs' do
+    it 'downloads a remote_file from Sauce Labs' do
+      expect(chef_run).to create_remote_file(::File.join(Chef::Config[:file_cache_path], 'Sauce-Connect-3.14159.zip'))
+    end
+    it 'notifies the extraction' do
+      resource = chef_run.remote_file(::File.join(Chef::Config[:file_cache_path], 'Sauce-Connect-3.14159.zip'))
+      expect(resource).to notify('execute[unzip-saucelabs-proxy]').to(:run).immediately
+      expect(resource).to_not notify('execute[unzip-saucelabs-proxy]').to(:run).delayed
+    end
+    it 'only runs the extraction if notified' do
+      expect(chef_run).to_not run_execute('unzip-saucelabs-proxy')
+    end
   end
 
   it 'should start a service called sauceconnect' do
     expect(chef_run).to enable_service('sauceconnect')
     expect(chef_run).to start_service('sauceconnect')
+    expect(chef_run).to_not disable_service('sauceconnect')
+    expect(chef_run).to_not stop_service('sauceconnect')
   end
 end
